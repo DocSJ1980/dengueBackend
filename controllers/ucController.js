bb299bf72e0bdc8deb4e65835d975f83954db530
DocSJ1980 <docsaj@outlook.com>
2022-11-07 23:05:21 +0500

    Polio Microplan generation endpoint refactored

--- controllers/ucController.js
+++ controllers/ucController.js
@@ -112,30 +112,39 @@ export const batchUCs = async (req, res, next) => {
     }
 }
 
+//SIXTH ROUTE: Create polio micor-plan with team data from frontend
 export const polioMPGen = async (req, res, next) => {
+    //Destructuring team data from frontend
     const { teamData } = req.body
-    try {
-        let i = 0
-        while (i < teamData.length) {
-            const { aicNumber, teams, fixedTeam, transitTeams } = teamData[i]
-            const foundUC = await UC.findOne({ supervisor: req.user._id })
-            if (foundUC) {
+    //Finding UC from logged in user and checking if he is supervisor or not
+    const foundUC = await UC.findOne({ supervisor: req.user._id })
+    if (foundUC) {
+        //Creating aic, polioTeams and polioDays
+        try {
+            let i = 0
+            //Creating aics looping through teamData array
+            while (i < teamData.length) {
+                const { aicNumber, mobileTeams, fixedTeam, transitTeams } = teamData[i]
                 const newAic = await Aic.create({ aicNumber })
-                if (teams.length > 0) {
+                //Creating mobilePolioTeams looping on the length of mobileTeams array in teamData 
+                if (mobileTeams.length > 0) {
                     try {
-                        for (let j = 0; j < teams.length; j++) {
-                            const teamNo = teams[j]
+                        for (let j = 0; j < mobileTeams.length; j++) {
+                            const teamNo = mobileTeams[j]
                             const teamType = "Mobile"
                             const newPolioTeam = await PolioTeam.create({ teamNo, teamType })
+                            //Creating 5 days for each mobilePolioTeam
                             try {
                                 for (let dayNo = 1; dayNo < 6; dayNo++) {
                                     const newPolioDay = await PolioDay.create({ dayNo })
+                                    //Referencing created day in the loop to newPolioTeam in the loop and saving the team 
                                     newPolioTeam.polioDays.push(newPolioDay._id)
                                     await newPolioTeam.save()
                                 }
                             } catch (error) {
                                 return next(new ErrorResponse("Failed to create PolioDays", 404))
                             }
+                            //Referencing newPolioTeam along with created and referenced days in the above loop to newAic in the loop and saving aic 
                             newAic.polioTeams.mobilePolioTeams.push(newPolioTeam._id)
                             await newAic.save()
                         }
@@ -143,28 +152,30 @@ export const polioMPGen = async (req, res, next) => {
                         return next(new ErrorResponse("Failed to create Mobile PolioTeam", 404))
                     }
                 }
+                //Creating fixedPolioTeams looping on the length of fixedTeams array in teamData 
                 if (fixedTeam.length > 0) {
                     try {
+                        //Referencing newPolioTeam to newAic in the loop and saving aic 
                         for (let k = 0; k < fixedTeam.length; k++) {
                             const teamNo = fixedTeam[k]
                             const teamType = "Fixed"
                             const newPolioTeam = await PolioTeam.create({ teamNo, teamType })
                             newAic.polioTeams.fixedPolioTeams.push(newPolioTeam._id)
                             await newAic.save()
-                            console.log("FixedTeam block completed successfully")
                         }
                     } catch (error) {
                         return next(new ErrorResponse("Failed to create Fixed PolioTeam", 404))
                     }
                 }
+                //Creating transitPolioTeams looping on the length of transitTeams array in teamData 
                 if (transitTeams.length > 0) {
                     try {
+                        //Referencing newPolioTeam to newAic in the loop and saving aic 
                         for (let l = 0; l < transitTeams.length; l++) {
                             console.log("transitTeams block reached")
                             const teamNo = transitTeams[l]
                             const teamType = "Transit"
                             const newPolioTeam = await PolioTeam.create({ teamNo, teamType })
-                            console.log("transitTeams created")
                             newAic.polioTeams.transitPolioTeams.push(newPolioTeam._id)
                             await newAic.save()
                         }
@@ -172,15 +183,16 @@ export const polioMPGen = async (req, res, next) => {
                         return next(new ErrorResponse("Failed to create Transit PolioTeam", 404))
                     }
                 }
-                // const createdSubUCs = { aic, polioTeams }
+                // pushing newAic in the loop to foundUC and saving UC 
                 foundUC.polioSubUCs.aic.push(newAic._id)
                 await foundUC.save()
+                i++
             }
-            // console.log(foundUC)
-            i++
+            return res.status(200).json("Area Incharges and mobile, transit and fixed teams created successfully")
+        } catch (error) {
+            return next(new ErrorResponse("Failed to Generate Polio Micro-Plan", 400))
         }
-        return res.status(200).json("Endpoint reached")
-    } catch (error) {
-        return next(new ErrorResponse("Failed to Generate Polio Micro-Plan", 400))
+    } else {
+        return res.status(404).json("You are not supervisor of requested UC")
     }
 }
